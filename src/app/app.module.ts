1948cd37ba677161605d95b2f1430757f5c07d93
Martin REMY <martin.remy.42@gmail.com>
2019-12-16 13:14:19 +0100

    remove ?deprecated HttpClient as recommander by migration-instructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {HttpModule} from '@angular/http';
+// import {HttpModule} from '@angular/http';
 import {RouterModule, Route} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from '../material-modules';
