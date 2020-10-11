076719f9b027d74fd0372fd24cd386325b56bc39
Quentin Stiévenart <quentin.stievenart@gmail.com>
2020-10-11 23:41:58 +0200

    Use ArrayUtils.mapBiArray

--- src/app/games/kamisado/KamisadoBoard.ts
+++ src/app/games/kamisado/KamisadoBoard.ts
@@ -3,16 +3,16 @@ import { KamisadoPiece } from "./KamisadoPiece";
 
 export class KamisadoBoard {
     public static SIZE: number = 8;
-    private static COLORS: ReadonlyArray<ReadonlyArray<KamisadoColor>> = [
-        [1, 2, 3, 4, 5, 6, 7, 8].map(KamisadoColor.of),
-        [6, 1, 4, 7, 2, 5, 8, 3].map(KamisadoColor.of),
-        [7, 4, 1, 6, 3, 8, 5, 2].map(KamisadoColor.of),
-        [4, 3, 2, 1, 8, 5, 4, 3].map(KamisadoColor.of),
-        [5, 6, 7, 8, 1, 2, 3, 4].map(KamisadoColor.of),
-        [2, 5, 8, 3, 6, 1, 4, 7].map(KamisadoColor.of),
-        [3, 8, 5, 2, 7, 4, 1, 6].map(KamisadoColor.of),
-        [8, 7, 6, 5, 4, 3, 2, 1].map(KamisadoColor.of)
-    ]
+    private static COLORS: ReadonlyArray<ReadonlyArray<KamisadoColor>> = ArrayUtils.mapBiArray([
+        [1, 2, 3, 4, 5, 6, 7, 8],
+        [6, 1, 4, 7, 2, 5, 8, 3],
+        [7, 4, 1, 6, 3, 8, 5, 2],
+        [4, 3, 2, 1, 8, 5, 4, 3],
+        [5, 6, 7, 8, 1, 2, 3, 4],
+        [2, 5, 8, 3, 6, 1, 4, 7],
+        [3, 8, 5, 2, 7, 4, 1, 6],
+        [8, 7, 6, 5, 4, 3, 2, 1]
+    ], KamisadoColor.of);
     public static getColorAt(x: number, y: number): KamisadoColor {
         return KamisadoBoard.COLORS[y][x];
     }
