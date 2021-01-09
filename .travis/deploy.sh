651436e539e2ab2983ab7d3f8017f176ab4c35ce
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-01-09 17:54:55 +0100

    [travis] disable build on PRs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+openssl aes-256-cbc -K $encrypted_bdd73078a253_key -iv $encrypted_bdd73078a253_iv -in .travis/firebaseConfig.ts.enc -out src/app/firebaseConfig.ts -d
+openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in .travis/id_rsa.enc -out id_rsa -d
+npx ng build
+sh .travis/deploy.sh
+
 eval "$(ssh-agent -s)"
 chmod 600 id_rsa
 ssh-add id_rsa
