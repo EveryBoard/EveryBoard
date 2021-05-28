e52faaeb6a086ff5b683fad3ef8143230bee0b39
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-05-28 07:07:21 +0200

    Improve script to update images and update the images

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+grep "new GameInfo" src/app/components/normal-component/pick-game/pick-game.component.ts | sed "s/.*new GameInfo([^,]*, '\([^']*\)'.*/\1/" > scripts/games.txt
 python scripts/screenshot.py
 mv *.png src/assets/images/
+rm scripts/games.txt
