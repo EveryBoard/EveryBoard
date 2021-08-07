17651d5563673d24919b0a74da470d7d854676ef
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-08-07 08:05:53 +0200

    [i18n]  add script to check validity of the translations, apply fixes

--- scripts/check-translations.sh
+++ scripts/check-translations.sh
@@ -3,3 +3,13 @@ grep "trans-unit id=" translations/messages.xlf > orig.txt
 grep "trans-unit id=" translations/messages.fr.xlf > new.txt
 diff orig.txt new.txt
 rm -f orig.txt new.txt
+
+SOURCES=$(grep "<source>" translations/messages.fr.xlf | wc -l)
+TARGETS=$(grep "<target>" translations/messages.fr.xlf | wc -l)
+if [[ "$SOURCES" -gt "$TARGETS" ]]; then
+    echo "There are $SOURCES <source> tags but only $TARGETS <target> tags!"
+    grep -n "</source>" translations/messages.fr.xlf -A1 | grep "</trans" -B1
+elif [[ "$SOURCES" -lt "$TARGETS" ]]; then
+    echo "There are $TARGETS <target> tags but only $SOURCES <source> tags!"
+    grep -n "<target>" translations/messages.fr.xlf -B1 | grep -e "[0-9]\+-" | grep -v "</source>"
+fi
