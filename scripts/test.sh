7e3222255dd679b763ab3a159b211169d9116e90
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-09-04 17:21:49 +0200

    [dao] Use script to run tests in CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-npx firebase emulators:exec --only firestore --project 'testing' 'ng test --code-coverage'
+npx firebase emulators:exec --only firestore --project 'testing' "ng test --code-coverage $@"
