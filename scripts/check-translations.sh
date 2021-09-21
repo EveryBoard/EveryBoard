05e1e03915b73e9b2f8f69b86138bf0f1f34d3c0
Martin Remy <martin.remy.42@gmail.com>
2021-09-21 22:27:55 +0200

    fixed translation

--- scripts/check-translations.sh
+++ scripts/check-translations.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./scripts/check-translations.py
+python ./scripts/check-translations.py
 if [ "$?" -eq 0 ]; then
     echo 'Translations are OK!'
     npx xlf-merge translations/messages.fr.xlf --convert json -o src/assets/fr.json
