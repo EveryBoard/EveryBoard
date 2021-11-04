0ea01e6b45a89a403e52290a81a2db050f218607
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-11-04 21:57:40 +0100

    [test-auth] karma strangeness

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
             check: {
                 global: {
                     statements: 98.49,
-                    branches: 96.56, // always keep it 0.02% below local coverage
+                    branches: 96.55, // always keep it 0.02% below local coverage
                     functions: 98.46,
                     lines: 98.50,
                 },
