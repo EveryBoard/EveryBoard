1d7b202b8dfe7c8e3ed857fbb5e27b9edbcc08ee
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-11-23 08:36:56 +0100

    [diam] Get back coverage in pylos (why though?)

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -23,10 +23,10 @@ module.exports = function(config) {
             ],
             check: {
                 global: {
-                    statements: 98.53,
-                    branches: 96.60, // always keep it 0.02% below local coverage
-                    functions: 98.52,
-                    lines: 98.55,
+                    statements: 98.58,
+                    branches: 96.72, // always keep it 0.02% below local coverage
+                    functions: 98.57,
+                    lines: 98.6,
                 },
             },
         },
