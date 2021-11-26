fc94cbe58bcbcf7ea86af36a9c18de30bd1912bc
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-11-26 21:06:07 +0100

    [minimax] Fix alpha beta pruning

--- src/app/games/p4/tests/P4Minimax.spec.ts
+++ src/app/games/p4/tests/P4Minimax.spec.ts
@@ -7,16 +7,38 @@ import { P4Rules } from '../P4Rules';
 describe('P4Minimax', () => {
 
     let minimax: P4Minimax;
+    let rules: P4Rules;
 
     beforeEach(() => {
-        const rules: P4Rules = new P4Rules(P4State);
+        rules = new P4Rules(P4State);
         minimax = new P4Minimax(rules, 'P4Minimax');
     });
-    xit('First choice should be center at all IA depths', () => {
+    it('First choice should be center at all IA depths', () => {
         const initialState: P4State = P4State.getInitialState();
         for (let depth: number = 1; depth < 6; depth ++) {
             const node: MGPNode<P4Rules, P4Move, P4State> = new MGPNode(null, null, initialState);
             expect(node.findBestMove(depth, minimax)).toEqual(P4Move.THREE);
         }
+    });
+    it('Minimax should prune when instructed to do so', () => {
+        const getBoardValueSpy: jasmine.Spy = spyOn(minimax, 'getBoardValue').and.callThrough();
+        const getListMovesSpy: jasmine.Spy = spyOn(minimax, 'getListMoves').and.callThrough();
+
+        // Given the number of moves of a minimax without alpha-beta pruning
+        rules.node.findBestMove(3, minimax, false, false);
+        const callsToGetBoardValueWithoutPruning: number = getBoardValueSpy.calls.count();
+        getBoardValueSpy.calls.reset();
+        const callsToGetListMovesWithoutPruning: number = getListMovesSpy.calls.count();
+        getListMovesSpy.calls.reset();
+
+        // when computing the same information with alpha-beta pruning enabled
+        rules.node = new MGPNode(null, null, P4State.getInitialState());
+        rules.node.findBestMove(3, minimax, false, true);
+        const callsToGetBoardValueWithPruning: number = getBoardValueSpy.calls.count();
+        const callsToGetListMovesWithPruning: number = getListMovesSpy.calls.count();
+
+        // then the number of calls is strictly lower
+        expect(callsToGetBoardValueWithPruning).toBeLessThan(callsToGetBoardValueWithoutPruning);
+        expect(callsToGetListMovesWithPruning).toBeLessThan(callsToGetListMovesWithoutPruning);
     });
 });
