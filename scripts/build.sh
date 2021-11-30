d70c82d7dc55fde0701b7299a0c851b6da229ea2
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-11-30 07:30:24 +0100

    [deployment] Different approach for deployment script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 ARGS="$@"
 pip install lxml selenium pandas
-cp ./.netlify/firebaseConfig.ts src/app/
+echo "export const firebaseConfig = {
+    apiKey: $API_KEY,
+    authDomain: $AUTH_DOMAIN,
+    databaseURL: $DATABASE_URL,
+    projectId: $PROJECT_ID,
+    storageBucket: $STORAGE_BUCKET,
+    messagingSenderId: $MESSAGING_SENDER_ID,
+};" > src/app/firebaseConfig.ts
 python ./scripts/update-translations.sh
 bash ./scripts/check-translations.sh
 bash ./scripts/update-images.sh
