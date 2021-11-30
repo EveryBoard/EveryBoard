d02b7670fcfadc6e43f2510b623034642f799ef0
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-11-30 07:46:30 +0100

    [deployment] Fix more typos in deployment script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,14 @@
 ARGS="$@"
 pip install lxml selenium pandas
 echo "export const firebaseConfig = {
-    apiKey: $API_KEY,
-    authDomain: $AUTH_DOMAIN,
-    databaseURL: $DATABASE_URL,
-    projectId: $PROJECT_ID,
-    storageBucket: $STORAGE_BUCKET,
-    messagingSenderId: $MESSAGING_SENDER_ID,
+    apiKey: '$API_KEY',
+    authDomain: '$AUTH_DOMAIN',
+    databaseURL: $DATABASE_URL',
+    projectId: '$PROJECT_ID',
+    storageBucket: '$STORAGE_BUCKET',
+    messagingSenderId: '$MESSAGING_SENDER_ID',
 };" > src/app/firebaseConfig.ts
-python ./scripts/update-translations.sh
+bash ./scripts/update-translations.sh
 bash ./scripts/check-translations.sh
 bash ./scripts/update-images.sh
 npm run build $ARGS
