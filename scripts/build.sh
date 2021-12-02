169df28e2d113a034465ac60053ffd6b234b8df8
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-02 08:29:45 +0100

    [deployment] Really trying to get chromedriver to work

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ ARGS="$@"
 export CHROME_BIN="$CHROME_PATH"
 echo $CHROME_PATH
 ls -l $CHROME_PATH
-python3 -m pip install lxml selenium pandas webdriver-manager
+python3 -m pip install lxml selenium pandas chromedriver || exit
 echo "export const firebaseConfig = {
     apiKey: '$API_KEY',
     authDomain: '$AUTH_DOMAIN',
@@ -12,7 +12,7 @@ echo "export const firebaseConfig = {
     storageBucket: '$STORAGE_BUCKET',
     messagingSenderId: '$MESSAGING_SENDER_ID',
 };" > src/app/firebaseConfig.ts
-bash ./scripts/update-translations.sh
-bash ./scripts/check-translations.sh
-bash ./scripts/update-images.sh
-npm run build:netlify $ARGS
+bash ./scripts/update-images.sh || exit
+bash ./scripts/update-translations.sh || exit
+bash ./scripts/check-translations.sh || exit
+npm run build:netlify $ARGS || exit
