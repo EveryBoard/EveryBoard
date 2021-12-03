931fbb6f7f1c1ffd6014b52815299e6b8d756aea
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-03 22:07:37 +0100

    [dark-mode] First attempt at dark mode

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-npm start &
-sleep 100 # wait until NPM has started
-NPM="$!"
+#npm start &
+#sleep 100 # wait until NPM has started
+#NPM="$!"
 grep "new GameInfo" src/app/components/normal-component/pick-game/pick-game.component.ts | sed "s/.*new GameInfo([^,]*, '\([^']*\)'.*/\1/" > scripts/games.txt
 python scripts/screenshot.py
-kill $NPM
+#kill $NPM
 mv *.png src/assets/images/
 rm scripts/games.txt
