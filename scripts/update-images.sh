5911f5521254c44a09888c5009998955cd1ab875
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-03 08:26:25 +0100

    [deployment] Update CI workflows

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-npm start &
-sleep 100 # wait until NPM has started
-NPM="$!"
+echo 'Make sure you are running npm start in parallel! Press enter when it is the case'
+read
 grep "new GameInfo" src/app/components/normal-component/pick-game/pick-game.component.ts | sed "s/.*new GameInfo([^,]*, '\([^']*\)'.*/\1/" > scripts/games.txt
 python3 scripts/screenshot.py
-kill $NPM
 mv *.png src/assets/images/
 rm scripts/games.txt
