d526c79428bc1e14e23f9dfb558acde0de813598
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-12 10:24:35 +0100

    [dark-mode] Generate both dark and light images

--- scripts/update-images.sh
+++ scripts/update-images.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-#npm start &
-#sleep 100 # wait until NPM has started
-#NPM="$!"
 grep "new GameInfo" src/app/components/normal-component/pick-game/pick-game.component.ts | sed "s/.*new GameInfo([^,]*, '\([^']*\)'.*/\1/" > scripts/games.txt
 python scripts/screenshot.py
-#kill $NPM
-mv *.png src/assets/images/
+mv *.png src/assets/images/light/
+# Change theme by simply copying the CSS
+cp src/sass/dark.scss src/sass/light.scss
+sleep 5 # Need to wait a bit for ng to refresh
+python scripts/screenshot.py
+mv *.png src/assets/images/dark/
+# Restore the CSS
+git checkout src/sass/light.scss
 rm scripts/games.txt
