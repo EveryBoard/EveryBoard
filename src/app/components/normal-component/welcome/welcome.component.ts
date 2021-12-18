1c1011da794aae203bff79a7f2f89d0d9fdc4a63
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-18 08:34:38 +0100

    [dark-mode] Get rid of masonry

--- src/app/components/normal-component/welcome/welcome.component.ts
+++ src/app/components/normal-component/welcome/welcome.component.ts
@@ -10,17 +10,26 @@ import { faNetworkWired, faDesktop, faBookOpen, IconDefinition } from '@fortawes
     templateUrl: './welcome.component.html',
 })
 export class WelcomeComponent {
-    public readonly games: GameInfo[] = GameInfo.ALL_GAMES().filter((game: GameInfo) => game.display === true);
+    public readonly numberOfColumns: number = 4;
+    public readonly games: GameInfo[][] = [];
     public readonly theme: 'dark' | 'light';
-    public iconOnlineGame: IconDefinition = faNetworkWired;
-    public iconLocalGame: IconDefinition = faDesktop;
-    public iconTutorial: IconDefinition = faBookOpen;
+    public readonly iconOnlineGame: IconDefinition = faNetworkWired;
+    public readonly iconLocalGame: IconDefinition = faDesktop;
+    public readonly iconTutorial: IconDefinition = faBookOpen;
 
     public constructor(private gameService: GameService,
                        private router: Router,
                        themeService: ThemeService) {
         this.theme = themeService.getTheme();
-
+        const allGames: GameInfo[] = GameInfo.ALL_GAMES().filter((game: GameInfo) => game.display === true);
+        let column: number = 0;
+        for (let i: number = 0; i < allGames.length; i++) {
+            if (i < this.numberOfColumns) {
+                this.games.push([]);
+            }
+            this.games[column].push(allGames[i]);
+            column = (column+1) % this.numberOfColumns;
+        }
     }
     public async createGame(game: string): Promise<boolean> {
         return this.gameService.createGameAndRedirectOrShowError(game);
