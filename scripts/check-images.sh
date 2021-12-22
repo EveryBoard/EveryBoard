ef20cbd80492e6d17faa93b7c7e1caadb0fc76c5
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-22 07:31:06 +0100

    [deployment] Update translations, update image script

--- scripts/check-images.sh
+++ scripts/check-images.sh
@@ -2,8 +2,12 @@
 grep "new GameInfo" src/app/components/normal-component/pick-game/pick-game.component.ts | sed "s/.*new GameInfo([^,]*, '\([^']*\)'.*/\1/" > scripts/games.txt
 FAILED=0
 for GAME in $(cat scripts/games.txt); do
-    if [ ! -f src/assets/images/$GAME.png ]; then
-        echo "Image is missing for game $GAME"
+    if [ ! -f src/assets/images/dark/$GAME.png ]; then
+        echo "Image is missing for game $GAME (dark theme)"
+        FAILED=1
+    fi
+    if [ ! -f src/assets/images/light/$GAME.png ]; then
+        echo "Image is missing for game $GAME (light theme)"
         FAILED=1
     fi
 done
