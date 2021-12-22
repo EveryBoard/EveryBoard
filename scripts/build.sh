68e89d45bd7e7ab3a933fb1072d275cd028c59b8
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-12-22 21:24:11 +0100

    Revert "[deployment] Add build script for netlify"
    This reverts commit 3688b2e9f61037896170d263fa06cfd422981640.

--- scripts/build.sh
+++
@@ -1,2 +0,0 @@
-echo "export const firebaseConfig = { apiKey: '$API_KEY', authDomain: '$AUTH_DOMAIN', databaseURL: '$DATABASE_URL', projectId: '$PROJECT_ID', storageBucket: '$STORAGE_BUCKET', messagingSenderId: '$MESSAGING_SENDER_ID' };" > src/app/firebaseConfig.ts
-npm run build:netlify
