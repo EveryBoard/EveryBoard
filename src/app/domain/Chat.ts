4b52e5a406cb55e9f07a05f3f7b94c4aed50c794
Quentin Stiévenart <quentin.stievenart@gmail.com>
2022-04-01 19:13:34 +0200

    [chat-security] PR comment: add comment

--- src/app/domain/Chat.ts
+++ src/app/domain/Chat.ts
@@ -1,6 +1,7 @@
 import { FirebaseDocument } from '../dao/FirebaseFirestoreDAO';
 import { FirebaseJSONObject } from '../utils/utils';
 
+// A chat has no object, it only contains a sub collection with the messages
 export type Chat = FirebaseJSONObject
 
 export type ChatDocument = FirebaseDocument<Chat>
