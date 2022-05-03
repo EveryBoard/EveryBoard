3256749a98e7ca98483bb5b0f7c7f690de711cb2
Quentin Stiévenart <quentin.stievenart@gmail.com>
2022-05-03 15:17:11 +0200

    [security-firebase] Further improvements to joiner security

--- src/app/dao/tests/JoinerDAO.spec.ts
+++ src/app/dao/tests/JoinerDAO.spec.ts
@@ -10,6 +10,7 @@ import { JoinerMocks } from 'src/app/domain/JoinerMocks.spec';
 import { MinimalUser } from 'src/app/domain/MinimalUser';
 import { UserDAO } from '../UserDAO';
 import { FirstPlayer, Joiner, PartStatus, PartType } from 'src/app/domain/Joiner';
+import { MGPOptional } from 'src/app/utils/MGPOptional';
 
 describe('JoinerDAO', () => {
 
@@ -82,7 +83,7 @@ describe('JoinerDAO', () => {
 
         await signOut();
         // and a non-verified user
-        const token: string = '{"sub": "bar@bar.com", "email": "bar@bar.com", "email_verified": true}';
+        const token: string = '{"sub": "bar@bar.com", "email": "bar@bar.com", "email_verified": false}';
         const credential: FireAuth.UserCredential =
             await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
                                                 FireAuth.GoogleAuthProvider.credential(token));
@@ -166,18 +167,154 @@ describe('JoinerDAO', () => {
             await expectFirebasePermissionDenied(result);
         }
     });
-    it('should allow non-creator to change status only from PROPOSED to STARTED', async() => {
+    it('should allow verified users to add themselves to candidates', async() => {
+        // Given a part, and a user (that is not the creator)
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        await joinerDAO.set(partId, { ...JoinerMocks.INITIAL, creator });
+        await signOut();
+
+        await createConnectedGoogleUser(true, 'bar@bar.com', 'candidate');
+        // When adding itself to the candidate list
+        const result: Promise<void> = joinerDAO.update(partId, { candidates: ['candidate'] });
+        // Then it should succeed
+        await expectAsync(result).toBeResolvedTo();
     });
     it('should forbid setting a fake username as a candidate', async() => {
+        // TODO: we currently can't ensure that
+        // Given a part, and a user (that is not the creator)
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        await joinerDAO.set(partId, { ...JoinerMocks.INITIAL, creator });
+        await signOut();
+
+        await createConnectedGoogleUser(true, 'bar@bar.com', 'candidate');
+        // When adding another user name to the candidate list
+        const result: Promise<void> = joinerDAO.update(partId, { candidates: ['not-myself'] });
+        // Then it should fail
+        await expectFirebasePermissionDenied(result);
     });
-    it('should allow verified users to add themselves to candidates', async() => {
+    xit('should forbid verified users to change other candidate when adding themselves', async() => {
+        // TODO We currently can't handle that
     });
-    it('should forbid verified users to change other candidate when adding themselves', async() => {
+    it('should allow chosenPlayer non-creator to change status only from PROPOSED to STARTED', async() => {
+        // Given a part with a joiner where the current user is set as chosenPlayer
+
+        // We have to follow a realistic workflow to achieve that
+        // The part is first created
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        await joinerDAO.set(partId, { ...JoinerMocks.INITIAL, creator });
+        await signOut();
+
+        // A candidate add itself to the candidates list
+        await createConnectedGoogleUser(true, 'bar@bar.com', 'candidate');
+        await expectAsync(joinerDAO.update(partId, { candidates: ['candidate'] })).toBeResolvedTo();
+        await signOut();
+
+        // The creator then selects candidates as chosenPlayer
+        const creatorToken: string = '{"sub": "foo@bar.com", "email": "foo@bar.com", "email_verified": true}';
+        await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
+                                            FireAuth.GoogleAuthProvider.credential(creatorToken));
+        const update: Partial<Joiner> = { chosenPlayer: 'candidate', partStatus: PartStatus.CONFIG_PROPOSED.value };
+        await expectAsync(joinerDAO.update(partId, update)).toBeResolvedTo();
+        await signOut();
+
+        // When the candidate accepts the config by setting partStatus
+        const candidateToken: string = '{"sub": "bar@bar.com", "email": "bar@bar.com", "email_verified": true}';
+        await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
+                                            FireAuth.GoogleAuthProvider.credential(candidateToken));
+        const result: Promise<void> = joinerDAO.update(partId, { partStatus: PartStatus.PART_STARTED.value });
+
+        // Then it should succeed
+        await expectAsync(result).toBeResolvedTo();
+    });
+    it('should forbid non-chosen player to change status from PROPOSED to STARTED', async() => {
+        // Given a part with a joiner where the current user is set as chosenPlayer
+
+        // We have to follow a realistic workflow to achieve that
+        // The part is first created
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        await joinerDAO.set(partId, { ...JoinerMocks.INITIAL, creator });
+        await signOut();
+
+        // A candidate add itself to the candidates list
+        await createConnectedGoogleUser(true, 'bar@bar.com', 'candidate');
+        await expectAsync(joinerDAO.update(partId, { candidates: ['candidate'] })).toBeResolvedTo();
+        await signOut();
+
+        // The creator then selects candidates as chosenPlayer
+        const creatorToken: string = '{"sub": "foo@bar.com", "email": "foo@bar.com", "email_verified": true}';
+        const update: Partial<Joiner> = { chosenPlayer: 'candidate', partStatus: PartStatus.CONFIG_PROPOSED.value };
+        await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
+                                            FireAuth.GoogleAuthProvider.credential(creatorToken));
+        await expectAsync(joinerDAO.update(partId, { chosenPlayer: 'candidate' })).toBeResolvedTo();
+        await signOut();
+
+        // When another player tries to start the part
+        await createConnectedGoogleUser(true, 'baz@bar.com', 'other');
+        const result: Promise<void> = joinerDAO.update(partId, { partStatus: PartStatus.PART_STARTED.value });
+
+        // Then it should fail
+        await expectFirebasePermissionDenied(result);
     });
     it('should forbid non-verified user to add themselves to candidates', async() => {
+        // Given a part, and a non-verified user
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        const joiner: Joiner = { ...JoinerMocks.INITIAL, creator };
+        await joinerDAO.set(partId, joiner);
+        await signOut();
+
+        const token: string = '{"sub": "bar@bar.com", "email": "bar@bar.com", "email_verified": false}';
+        const credential: FireAuth.UserCredential =
+            await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
+                                                FireAuth.GoogleAuthProvider.credential(token));
+        await userDAO.set(credential.user.uid, { verified: false, username: 'user' });
+        // When adding the non-verified user to the candidates
+        const result: Promise<void> = joinerDAO.update(partId, { candidates: ['user'] });
+        // Then it should fail
+        await expectFirebasePermissionDenied(result);
+
     });
     it('should allow verified users to read the joiner', async() => {
+        // Given a part, and a verified user
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        const joiner: Joiner = { ...JoinerMocks.INITIAL, creator };
+        await joinerDAO.set(partId, joiner);
+        await signOut();
+
+        await createConnectedGoogleUser(true, 'bar@bar.com', 'candidate');
+        // When reading the joiner
+        const result: Promise<MGPOptional<Joiner>> = joinerDAO.read(partId);
+        // Then it succeeds
+        await expectAsync(result).toBeResolvedTo(MGPOptional.of(joiner));
     });
     it('should forbid non-verified users to read the joiner', async() => {
+        // Given a part, and a non-verified user
+        const creatorUser: FireAuth.User = await createConnectedGoogleUser(true, 'foo@bar.com', 'creator');
+        const creator: MinimalUser = { id: creatorUser.uid, name: 'creator' };
+        const partId: string = await partDAO.create(PartMocks.INITIAL);
+        const joiner: Joiner = { ...JoinerMocks.INITIAL, creator };
+        await joinerDAO.set(partId, joiner);
+        await signOut();
+
+        const token: string = '{"sub": "bar@bar.com", "email": "bar@bar.com", "email_verified": false}';
+        const credential: FireAuth.UserCredential =
+            await FireAuth.signInWithCredential(TestBed.inject(FireAuth.Auth),
+                                                FireAuth.GoogleAuthProvider.credential(token));
+        await userDAO.set(credential.user.uid, { verified: false, username: 'user' });
+        // When reading the joiner
+        const result: Promise<MGPOptional<Joiner>> = joinerDAO.read(partId);
+        // Then it should fail
+        await expectFirebasePermissionDenied(result);
     });
 });
