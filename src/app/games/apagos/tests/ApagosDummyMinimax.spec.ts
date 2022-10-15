70f20da39f6f0d4b94009466c96596b1f1e228b5
Quentin Stiévenart <quentin.stievenart@gmail.com>
2022-10-15 07:16:05 +0200

    [apagos-coverage] Improve coverage of Apagos minimax

--- src/app/games/apagos/tests/ApagosDummyMinimax.spec.ts
+++ src/app/games/apagos/tests/ApagosDummyMinimax.spec.ts
@@ -1,4 +1,7 @@
 /* eslint-disable max-lines-per-function */
+import { Player } from 'src/app/jscaip/Player';
+import { RulesUtils } from 'src/app/jscaip/tests/RulesUtils.spec';
+import { MGPOptional } from 'src/app/utils/MGPOptional';
 import { ApagosDummyMinimax } from '../ApagosDummyMinimax';
 import { ApagosMove } from '../ApagosMove';
 import { ApagosNode, ApagosRules } from '../ApagosRules';
@@ -24,5 +27,28 @@ describe('ApagosDummyMinimax', () => {
         expect(moves.length).toBe(8);
         const isThereTransfer: boolean = moves.some((move: ApagosMove) => move.isDrop() === false);
         expect(isThereTransfer).toBeFalse();
+    });
+    it('should consider having advantage has a higher value board', () => {
+        // Given two states each with more pieces of one specific player
+        const weakerState: ApagosState = ApagosState.fromRepresentation(0, [
+            [0, 0, 0, 0],
+            [7, 0, 0, 1],
+            [7, 5, 3, 1],
+        ], 10, 2);
+        const strongerState: ApagosState = ApagosState.fromRepresentation(0, [
+            [7, 0, 0, 1],
+            [0, 0, 0, 0],
+            [7, 5, 3, 1],
+        ], 2, 10);
+
+        // When computing the value of the boards
+        // Then the state with the most pieces of the player should have a higher value
+        RulesUtils.expectSecondStateToBeBetterThanFirstFor(minimax,
+                                                           weakerState,
+                                                           MGPOptional.empty(),
+                                                           strongerState,
+                                                           MGPOptional.empty(),
+                                                           Player.ZERO);
+
     });
 });
