d2dbc753d89c00b3b116231037138f4017b4963f
Quentin Stiévenart <quentin.stievenart@gmail.com>
2022-10-16 21:09:55 +0200

    [abalone-minimax-cover] Fix GWT in abalone dummy minimax

--- src/app/games/abalone/tests/AbaloneDummyMinimax.spec.ts
+++ src/app/games/abalone/tests/AbaloneDummyMinimax.spec.ts
@@ -19,12 +19,12 @@ describe('AbaloneDummyMinimax', () => {
     beforeEach(() => {
         minimax = new AbaloneDummyMinimax(new AbaloneRules(AbaloneState), 'dummy');
     });
-    it('should propose all non-suicidal moved at first turn, there is 42', () => {
+    it('should propose all non-suicidal moved at first turn, there is 44', () => {
         // given initial node
         const initialState: AbaloneState = AbaloneState.getInitialState();
         const initialNode: AbaloneNode = new AbaloneNode(initialState);
 
-        // then we should have 42 moves
+        // then we should have 44 moves
         expect(minimax.getListMoves(initialNode).length).toEqual(44);
     });
     it('should include pushing moves', () => {
@@ -43,7 +43,7 @@ describe('AbaloneDummyMinimax', () => {
         const state: AbaloneState = new AbaloneState(board, 0);
         const node: AbaloneNode = new AbaloneNode(state);
 
-        // then we should have 42 moves
+        // then we should have 15 moves
         expect(minimax.getListMoves(node).length).toEqual(15);
     });
     it('should assign a higher score when one has more pieces on board', () => {
