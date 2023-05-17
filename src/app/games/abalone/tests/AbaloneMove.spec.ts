dcdd7c41cf130f5971e3b762712ad0d9e7f893e6
Quentin Stievenart <quentin.stievenart@gmail.com>
2023-05-17 15:31:08 -0400

    [remove-number-encoders] Abalone

--- src/app/games/abalone/tests/AbaloneMove.spec.ts
+++ src/app/games/abalone/tests/AbaloneMove.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-lines-per-function */
 import { Coord } from 'src/app/jscaip/Coord';
 import { HexaDirection } from 'src/app/jscaip/HexaDirection';
-import { NumberEncoderTestUtils } from 'src/app/utils/tests/Encoder.spec';
+import { EncoderTestUtils } from 'src/app/utils/tests/Encoder.spec';
 import { MGPFallible } from 'src/app/utils/MGPFallible';
 import { AbaloneDummyMinimax } from '../AbaloneDummyMinimax';
 import { AbaloneState } from '../AbaloneState';
@@ -63,7 +63,7 @@ describe('AbaloneMove', () => {
         const minimax: AbaloneDummyMinimax = new AbaloneDummyMinimax(rules, 'dummy');
         const firstTurnMoves: AbaloneMove[] = minimax.getListMoves(rules.node);
         for (const move of firstTurnMoves) {
-            NumberEncoderTestUtils.expectToBeBijective(AbaloneMove.encoder, move);
+            EncoderTestUtils.expectToBeBijective(AbaloneMove.encoder, move);
         }
     });
 });
