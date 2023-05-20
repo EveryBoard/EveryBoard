56292114d05a40a10182cfca51aed79845e76576
Quentin Stievenart <quentin.stievenart@gmail.com>
2023-05-20 12:39:35 -0400

    [NewGameHelper] Expand new game README

--- src/app/games/new-game/README.md
+++ src/app/games/new-game/README.md
@@ -7,7 +7,12 @@ To create a new game named `foo`:
   - Implement the game (see [recommended order](#recommended-order-of-definition) below)
     while at the same time developing tests for it.
     The source files in this directory should contain enough documentation to implement most games.
-
+  - Import `FooComponent` in `src/app/app.module.ts`, in the alphabetical order with the other games components, and add it to the `declarations` section of the `@NgModule` directive.
+  - Import `FooComponent` in `pick-game.component.spec.ts`, following the alphabetical order. Add it at the end of `GameInfo.ALL_GAMES`
+  - For each method in the game component that can be activated by user interaction, they should check that the current player is allowed to interact with the component by calling `this.canUserPlay(id)` where `id` is the id of the HTML element that was interacted with. This method returns a `MGPValidation` that should be passed to `cancelMove` in case of failure.
+  - List these interaction methods in `GameComponent.spec.ts`, in the test called `clicks method should fail when an observer clicks`.
+  
+It is recommended to look at existing game's implementation for more details.
 
 # Recommended order of definition
 Recommended order to define your game:
---
+++ src/app/games/new-game/tests/NewGameDummyMinimax.spec.ts
@@ -0,0 +1,38 @@
+import { Player } from 'src/app/jscaip/Player';
+import { RulesUtils } from 'src/app/jscaip/tests/RulesUtils.spec';
+import { MGPOptional } from 'src/app/utils/MGPOptional';
+import { NewGameDummyMinimax } from '../NewGameDummyMinimax';
+import { NewGameMove } from '../NewGameMove';
+import { NewGameNode, NewGameRules } from '../NewGameRules';
+import { NewGameState } from '../NewGameState';
+
+/**
+ * These are the tests for the minimax.
+ * We want to test two things: the list of possible moves and the scoring function.
+ */
+describe('NewGameDummyMinimax', () => {
+
+    let minimax: NewGameDummyMinimax;
+
+    beforeEach(() => {
+        minimax = new NewGameDummyMinimax(NewGameRules.get(), 'NewGameDummyMinimax');
+    });
+    it('should have all move options', () => {
+        // Given an initial node
+        const initialState: NewGameState = NewGameState.getInitialState();
+        const node: NewGameNode = new NewGameNode(initialState);
+
+        // When computing the list of moves
+        const moves: NewGameMove[] = minimax.getListMoves(node);
+
+        // Then there should be this many moves
+        expect(moves.length).toBe(0);
+    });
+    it('should have some score', () => {
+        /**
+         * To test scores, most of the time you want to rely on `RulesUtils.expectSecondStateToBeBetterThanFirstFor`.
+         */
+        const initialState: NewGameState = NewGameState.getInitialState();
+        RulesUtils.expectStatesToBeOfEqualValue(minimax, initialState, initialState);
+    });
+});
