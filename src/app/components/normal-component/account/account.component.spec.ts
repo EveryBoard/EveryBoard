acac8b4f809c1ef5fd844b03e7a44299cbc1e28c
Quentin Stievenart <quentin.stievenart@gmail.com>
2023-05-21 11:29:45 -0400

    [change-password] Fix tests, add translations

--- src/app/components/normal-component/account/account.component.spec.ts
+++ src/app/components/normal-component/account/account.component.spec.ts
@@ -1,15 +1,15 @@
 /* eslint-disable max-lines-per-function */
-import { fakeAsync, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { UserMocks } from 'src/app/domain/UserMocks.spec';
 import { ConnectedUserServiceMock } from 'src/app/services/tests/ConnectedUserService.spec';
-import { expectValidRouting, SimpleComponentTestUtils } from 'src/app/utils/tests/TestUtils.spec';
+import { expectValidRoutingLink, SimpleComponentTestUtils } from 'src/app/utils/tests/TestUtils.spec';
 import { ResetPasswordComponent } from '../reset-password/reset-password.component';
 import { AccountComponent } from './account.component';
 
-fdescribe('AccountComponent', () => {
-    let testUtils: SimpleComponentTestUtils<AccountComponent>;
+describe('AccountComponent', () => {
 
+    let testUtils: SimpleComponentTestUtils<AccountComponent>;
 
     beforeEach(fakeAsync(async() => {
         testUtils = await SimpleComponentTestUtils.create(AccountComponent);
@@ -20,14 +20,11 @@ fdescribe('AccountComponent', () => {
         expect(testUtils.getComponent()).toBeTruthy();
     });
     it('should redirect to reset password form when clicking on the corresponding button', fakeAsync(async() => {
-        const router: Router = TestBed.inject(Router);
-        spyOn(router, 'navigate').and.callThrough();
-
         // Given some connected user
         ConnectedUserServiceMock.setUser(UserMocks.CONNECTED_AUTH_USER);
-        // When clicking on the "reset password" link
-        testUtils.clickElement('#reset');
-        // Then it should redirect to the reset password component
-        expectValidRouting(router, ['/reset-password'], ResetPasswordComponent);
+        // When displaying the page
+        // Then it should show a reset link pointing to the reset password component
+        const button: DebugElement = testUtils.findElement('#reset');
+        expectValidRoutingLink(button, '/reset-password', ResetPasswordComponent);
     }));
 });
