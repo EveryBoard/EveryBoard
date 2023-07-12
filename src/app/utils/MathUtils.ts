62150dd1f4ae88d7a8352e3b75ce5a78e06a55e9
Quentin Stievenart <quentin.stievenart@gmail.com>
2023-07-12 08:02:55 -0400

    [gcd] Move GCD computation out of Vector

---
+++ src/app/utils/MathUtils.ts
@@ -0,0 +1,10 @@
+export class MathUtils {
+    /**
+     * Returns the greatest common divisor between two numbers a and b.
+     * Uses the euclidean algorithm. Negative inputs are supported.
+     */
+    public static gcd(a: number, b: number): number {
+        if (b === 0) return Math.abs(a);
+        return MathUtils.gcd(b, a % b);
+    }
+}
