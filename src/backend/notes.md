1f396000c1c97b1eea3770b798d66f5374ee0858
Quentin Stievenart <quentin.stievenart@gmail.com>
2023-10-15 14:13:10 -0400

    [backend] Get first running backend

--- src/backend/notes.md
+++ src/backend/notes.md
@@ -1,8 +1,18 @@
+# Next steps
+  - [X] How to launch the server? Just start with  a dumb server that receives an API call
+  - [ ] Write a simple test suite with jasmine that tests if the server is reachable
+  - [ ] Add firebase initializeApp
+  - [ ] How to do auth? Add simple auth to the server and that's it
+  - [ ] Then add more features (see server-old.ts)
+# Scripts
+  - Generate protobuf files: see Proto
+  - Launch backend: npx ts-node --esm ./src/backend/server
+# Examples
 exemple: https://github.com/CatsMiaow/node-grpc-typescript/
 
 better: https://www.giac.cc/posts/grpc-static-code-typescript/
 
-
+# Proto
 To compile the .proto:
 
 cd src/backend
@@ -11,8 +21,12 @@ npx grpc_tools_node_protoc --plugin=protoc-gen-ts=../../node_modules/.bin/protoc
 s_out=grpc_js:./out --js_out=import_style=commonjs:./out --grpc_out=grpc_js:./out -I protos ./protos/every
 board.proto
 
+ 
+    "protoc": "mkdir -p dist/backend && npx grpc_tools_node_protoc --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts --ts_out=grpc_js:./dist/backend --js_out=import_style=typescript:./dist/backend --grpc_out=grpc_js:./dist/backend -I ./src/backend/protos/ ./src/backend/protos/everyboard.proto",
+    
+    but now replaced with something different
 
-TODO:
+# Auth
   - how to do auth? We want to check who is making the request
     Check https://firebase.google.com/docs/auth/admin/verify-id-tokens
     1. get id token on the client
@@ -22,7 +36,6 @@ firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(id
 }).catch(function(error) {
   // Handle error
 });
-
 
     2. verify id token on the server
 // idToken comes from the client app
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,22 +1,7 @@
 import * as grpc from '@grpc/grpc-js';
 
+import { GameIdAndType, GameIdOrFailure, SuccessOrFailure, UnimplementedEveryboardService } from '../../dist/backend/everyboard';
 
-
-import { GameIdAndType, GameIdOrFailure } from './out/everyboard_pb';
-import { EveryboardService } from './out/everyboard_grpc_pb';
-import { User } from 'src/app/domain/User';
-import { Utils } from 'src/app/utils/utils';
-import { UserDAO } from 'src/app/dao/UserDAO';
-import { getFirestore } from 'firebase-admin/firestore';
-import { initializeApp } from 'firebase-admin/app';
-import { getAuth } from 'firebase-admin/auth';
-import { firebaseConfig } from 'src/app/firebaseConfig';
-import { PartDAO } from 'src/app/dao/PartDAO';
-import { Firestore } from 'firebase/firestore';
-import { MinimalUser } from 'src/app/domain/MinimalUser';
-import { MGPResult, Part } from 'src/app/domain/Part';
-import { ConfigRoom, FirstPlayer, PartStatus, PartType } from 'src/app/domain/ConfigRoom';
-import { ConfigRoomDAO } from 'src/app/dao/ConfigRoomDAO';
 
 type Call<X, Y> = grpc.ServerUnaryCall<X, Y>;
 type Reply<X> = grpc.sendUnaryData<X>;
@@ -24,21 +9,10 @@ type Reply<X> = grpc.sendUnaryData<X>;
 type GameId = string; // A game id, corresponding to the id of the firestore element
 type UserId = string; // A user id, corresponding to the user's uid
 
-type SuccessOrFailureBasedReply = { setSuccess: (success: boolean) => void, setErrormessage: (reason: string) => void };
-
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const userDAO: UserDAO = new UserDAO(firestore as unknown as Firestore);
-const partDAO: PartDAO = new PartDAO(firestore as unknown as Firestore);
-const configRoomDAO: ConfigRoomDAO = new ConfigRoomDAO(firestore as unknown as Firestore);
-
-async function checkAuthAndGetUid(idToken: string): Promise<string> {
-    return getAuth().verifyIdToken(idToken).then((decodedToken) => decodedToken.uid);
-}
-
-async function checkAuth(idToken: string): Promise<void> {
-    await checkAuthAndGetUid(idToken);
-}
+type SuccessOrFailureBasedReply = {
+    set success(success: boolean);
+    set errorMessage(reason: string);
+};
 
 /**
  * This runs some action in a promise.
@@ -46,66 +20,36 @@ async function checkAuth(idToken: string): Promise<void> {
  * Then, it runs the action.
  * In case the action fails (i.e., throws an error), it fills the reply with the error message.
  */
-async function tryOrFail<X extends SuccessOrFailureBasedReply>(idToken: string, callback: Reply<X>, reply: X, action: () => Promise<void>): Promise<void> {
+async function tryOrFail<X extends SuccessOrFailureBasedReply>(idToken: string, reply: X, action: () => Promise<void>): Promise<void> {
     const catchError = err => {
-        reply.setSuccess(false);
-        reply.setErrormessage(err.toString())
+        reply.success = false;
+        reply.errorMessage = err.toString()
     };
-    await checkAuth(idToken).catch(catchError);
+    // TODO await checkAuth(idToken).catch(catchError);
     return action().catch(catchError);
 }
 
-/**
- * This creates an unstarted game
- * It fails if:
- *   - the creator is already in another game
- * Performs 1 read + 3 write.
- */
-function initializeGame(call: Call<GameIdAndType, GameIdOrFailure>, callback: Reply<GameIdOrFailure>): Promise<void> {
-    const reply: GameIdOrFailure = new GameIdOrFailure();
-    return tryOrFail(call.request.getIdtoken(), callback, reply, async() => {
-        const userId: UserId = call.request.getIdtoken();
-        const user: User = (await userDAO.read(call.request.getIdtoken())).get(); // 1 read
-        if (user.currentGame !== null) {
-            throw new Error('You can be creator of only one game at a time');
-        }
-        const creator: MinimalUser = {
-            id: userId,
-            name: Utils.getNonNullable(user.username),
-        };
-        const typeGame: string = call.request.getType();
+class EveryboardService extends UnimplementedEveryboardService {
 
-        // Create the game
-        const newPart: Part = {
-            typeGame,
-            playerZero: creator,
-            turn: -1,
-            result: MGPResult.UNACHIEVED.value,
-        };
-        const gameId: string = await partDAO.create(newPart);
-        // Then the config room
-        const newConfigRoom: ConfigRoom = {
-            chosenOpponent: null,
-            firstPlayer: FirstPlayer.RANDOM.value,
-            partType: PartType.STANDARD.value,
-            partStatus: PartStatus.PART_CREATED.value,
-            maximalMoveDuration: PartType.NORMAL_MOVE_DURATION,
-            totalPartDuration: PartType.NORMAL_PART_DURATION,
-            creator,
-        };
-        return configRoomDAO.set(gameId, newConfigRoom);
-        // Then the chat
-
-        reply.setSuccess(true);
-        reply.setGameid(gameId);
-        callback(null, reply);
-    });
+    public initializeGame(call: Call<GameIdAndType, SuccessOrFailure>,
+                          callback: Reply<SuccessOrFailure>)
+    : Promise<void>
+    {
+        const reply: SuccessOrFailure = new SuccessOrFailure();
+        return tryOrFail(call.request.idToken, reply, async() => {
+            const userId: UserId = call.request.idToken;
+            reply.success = true;
+            callback(null, reply);
+        });
+    }
 }
 
 
 var server = new grpc.Server();
-server.addService(EveryboardService, { initializeGame });
+server.addService(EveryboardService.definition, new EveryboardService());
 
+console.log('STARTING')
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    console.log('STARTING 2')
   server.start();
 });
