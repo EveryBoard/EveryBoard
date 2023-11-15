b757d61ec2f152a1c8ca3f87ae70b3c0a7f2f445
Martin Remy <martin.remy.42@gmail.com>
2023-11-15 19:46:13 +0100

    [rook-like-move-highlight] fix Coord coverage

--- src/app/jscaip/tests/Coord.spec.ts
+++ src/app/jscaip/tests/Coord.spec.ts
@@ -10,11 +10,13 @@ describe('Coord', () => {
         expect(smallCoord.compareTo(bigCoord)).toBe(-1);
         expect(smallCoord.compareTo(smallCoord)).toBe(0);
     });
+
     it('should compare correctly obvious case', () => {
         const smallCoord: Coord = new Coord(0, 0);
         const bigCoord: Coord = new Coord(2, 2);
         expect(smallCoord.compareTo(bigCoord)).toBe(-1);
     });
+
     it('should override equals correctly', () => {
         const coord: Coord = new Coord(0, 0);
         const horizontalNeighbors: Coord = new Coord(1, 0);
@@ -23,6 +25,7 @@ describe('Coord', () => {
         expect(coord.equals(horizontalNeighbors)).toBeFalse();
         expect(coord.equals(verticalNeighbors)).toBeFalse();
     });
+
     it('should give correct coords between this and other coord', () => {
         const coord: Coord = new Coord(0, 0);
         const notAligned: Coord = new Coord(2, 1);
@@ -33,12 +36,14 @@ describe('Coord', () => {
         expect(coord.getCoordsToward(neighbors)).toEqual([]);
         expect(coord.getCoordsToward(alignedFar)).toEqual([new Coord(1, 1)]);
     });
+
     it('should throw when asked distance toward an unaligned coord', () => {
         const coord: Coord = new Coord(0, 0);
         const unalignedCoord: Coord = new Coord(1, 2);
         expect(() => coord.getDistance(unalignedCoord))
             .toThrowError('Cannot calculate distance with non aligned coords.');
     });
+
     it('should compute hexagonal alignments with isHexagonallyAlignedWith', () => {
         const coord: Coord = new Coord(1, 1);
         expect(coord.isHexagonallyAlignedWith(new Coord(0, 0))).toBeFalse();
@@ -46,19 +51,25 @@ describe('Coord', () => {
         expect(coord.isHexagonallyAlignedWith(new Coord(1, 4))).toBeTrue();
         expect(coord.isHexagonallyAlignedWith(new Coord(5, 4))).toBeFalse();
     });
+
     describe('getDirectionToward', () => {
+
         it('should give direction', () => {
             const center: Coord = new Coord(0, 0);
             const lowRight: Coord = new Coord(2, 2);
             expect(center.getDirectionToward(lowRight).get()).toEqual(Direction.DOWN_RIGHT);
         });
+
         it('should fail when given invalid direction', () => {
             const center: Coord = new Coord(0, 0);
             const lowRight: Coord = new Coord(2, 4);
             expect(() => center.getDirectionToward(lowRight).get()).toThrow();
         });
+
     });
+
     describe('getUntil', () => {
+
         it('should include all coord between A and B, A and B excluded', () => {
             // Given a coord B that is 3 steps after A
             const A: Coord = new Coord(0, 0);
@@ -74,5 +85,46 @@ describe('Coord', () => {
             const BIncluded: boolean = coords.some((c: Coord) => c.equals(B));
             expect(BIncluded).toBeFalse();
         });
+
     });
+
+    describe('getAllCoordsToward', () => {
+
+        it('should return only current coord when coord and end are identical', () => {
+            // Given any coord
+            const coord: Coord = new Coord(0, 0);
+
+            // When calling getAllCoordsToward with another coord identical
+            const allCoordsToward: Coord[] = coord.getAllCoordsToward(new Coord(0, 0));
+
+            // Then the resulting list should only have coord
+            expect(allCoordsToward).toEqual([coord]);
+        });
+
+        it('should return only coord and end when coord and end are neighbor', () => {
+            // Given any coord
+            const coord: Coord = new Coord(0, 0);
+
+            // When calling getAllCoordsToward with another neighbooring coord
+            const neighbor: Coord = new Coord(1, 1);
+            const allCoordsToward: Coord[] = coord.getAllCoordsToward(neighbor);
+
+            // Then the resulting list should only the two coord
+            expect(allCoordsToward).toEqual([coord, neighbor]);
+        });
+
+        it('should return all element of the traject when start and end are far', () => {
+            // Given any coord
+            const coord: Coord = new Coord(0, 0);
+
+            // When calling getAllCoordsToward with a far away aligned coord
+            const farAwayAlignedCoord: Coord = new Coord(2, 2);
+            const allCoordsToward: Coord[] = coord.getAllCoordsToward(farAwayAlignedCoord);
+
+            // Then the resulting list should only the two coord
+            expect(allCoordsToward).toEqual([coord, new Coord(1, 1), farAwayAlignedCoord]);
+        });
+
+    });
+
 });
