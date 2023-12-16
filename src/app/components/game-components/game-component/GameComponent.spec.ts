d525e32679a5fd3d393a5ce2856cf9e92b42dc17
Martin Remy <martin.remy.42@gmail.com>
2023-12-16 14:20:23 +0100

    [RenderPlayerAndFourStatePieceValueAttributePrivate] remove fit

--- src/app/components/game-components/game-component/GameComponent.spec.ts
+++ src/app/components/game-components/game-component/GameComponent.spec.ts
@@ -47,7 +47,7 @@ describe('GameComponent', () => {
         expect(result.getReason()).toEqual('GameComponent: ' + errorMessage);
         expect(ErrorLoggerService.logError).toHaveBeenCalledWith('GameComponent', errorMessage, errorData);
     }));
-    fit('click methods should refuse when observer clicks', fakeAsync(async() => {
+    it('click methods should refuse when observer clicks', fakeAsync(async() => {
         const clickableMethods: { [gameName: string]: { [methodName: string]: unknown[] } } = {
             Abalone: {
                 onPieceClick: [0, 0],
