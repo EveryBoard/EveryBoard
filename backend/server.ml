57046ebab8f1296d40fb2e777190729570ce2873
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-01-07 13:14:04 -0500

    [backend] Add Dockerfile, serve stats without requiring auth

--- backend/server.ml
+++ backend/server.ml
@@ -10,14 +10,13 @@ let api = [
     Dream.scope "/" [Token_refresher.middleware !Options.service_account_file; Auth.middleware]
     @@ List.concat [
       Game.routes;
-      Stats.routes;
     ];
   ]
 
 let start () =
-  Mirage_crypto_rng_lwt.initialize (); (* Required for token refresher and jwt *)
+  Mirage_crypto_rng_lwt.initialize (module Mirage_crypto_rng.Fortuna); (* Required for token refresher and JWT *)
   Dream.initialize_log ~level:`Debug ();
   Dream.run ~interface:!Options.address ~port:!Options.port
   @@ Dream.logger
   @@ Cors.middleware
-  @@ Dream.router api
+  @@ Dream.router (List.concat [Stats.routes; api])
