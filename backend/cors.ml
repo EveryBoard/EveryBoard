0144f4083b8dc8865218130d98dab472f6e2c687
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-01-18 20:25:09 -0500

    [backend] Improve CORS

--- backend/cors.ml
+++ backend/cors.ml
@@ -1,5 +1,14 @@
 open Utils
 
+let headers = [
+  ("Access-Control-Allow-Origin", !Options.frontend_origin);
+  ("Access-Control-Allow-Methods", "GET, POST, HEAD, PATCH, DELETE");
+  ("Access-Control-Allow-Headers", "Authorization");
+  ("Access-Control-Allow-Credentials", "true");
+  (* This enables browsers to cache this response for up to 24 hours, avoiding many OPTIONS requests *)
+  ("Access-Control-Max-Age", "86400");
+]
+
 (** This middleware is used for Cross-Origin Request Sharing (CORS). It lets the
     clients know which HTTP methods are authorized when they do preflight
     requests. We have to allow CORS requests because the backend and frontend
@@ -7,15 +16,10 @@ open Utils
 let middleware : Dream.middleware = fun handler request ->
   match Dream.method_ request with
   | `OPTIONS ->
-    let headers = [
-      ("Access-Control-Allow-Origin", !Options.frontend_origin);
-      ("Access-Control-Allow-Methods", "GET, POST, HEAD, PATCH, DELETE");
-      ("Access-Control-Allow-Headers", "Authorization");
-      (* This enables browsers to cache this response for up to 24 hours, avoiding many OPTIONS requests *)
-      ("Access-Control-Max-Age", "86400");
-    ] in
+    (* Upon a preflight request (OPTIONS), just send the headers *)
     Dream.empty ~headers `No_Content
   | _ ->
     let* response = handler request in
-    Dream.add_header response "Access-Control-Allow-Origin" !Options.frontend_origin;
+    (* We need to add the headers to any other response too *)
+    List.iter (fun (name, value) -> Dream.add_header response name value) headers;
     Lwt.return (response)
