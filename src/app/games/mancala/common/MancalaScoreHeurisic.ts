9e1e42b9537b3cc4c48ca651354d06c4da6c5290
Martin Remy <martin.remy.42@gmail.com>
2024-01-21 17:41:01 +0100

    [render-private-player-value] fixed linter

--- src/app/games/mancala/common/MancalaScoreHeurisic.ts
+++ src/app/games/mancala/common/MancalaScoreHeurisic.ts
@@ -2,8 +2,6 @@ import { MancalaState } from '../common/MancalaState';
 import { PlayerMetricHeuristic } from 'src/app/jscaip/AI/Minimax';
 import { PlayerNumberTable } from 'src/app/jscaip/PlayerNumberTable';
 import { MancalaMove } from './MancalaMove';
-import { PlayerNumberMap } from 'src/app/jscaip/PlayerMap';
-import { Player } from 'src/app/jscaip/Player';
 import { MancalaConfig } from './MancalaConfig';
 import { MGPOptional } from 'src/app/utils/MGPOptional';
 import { MancalaNode } from './MancalaRules';
