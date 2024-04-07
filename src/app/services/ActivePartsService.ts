a8866db7dc8d889ebdf52f343d69cd3213e7a991
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-04-07 17:28:04 -0400

    [active-parts-fix] Fix use of PartDocument in ActivePartsService

--- src/app/services/ActivePartsService.ts
+++ src/app/services/ActivePartsService.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { PartDAO } from '../dao/PartDAO';
 import { MGPResult, Part, PartDocument } from '../domain/Part';
 import { FirestoreCollectionObserver } from '../dao/FirestoreCollectionObserver';
+import { FirestoreDocument } from '../dao/FirestoreDAO';
 
 @Injectable({
     providedIn: 'root',
@@ -18,30 +19,35 @@ export class ActivePartsService {
     }
     public subscribeToActiveParts(callback: (parts: PartDocument[]) => void): Subscription {
         let activeParts: PartDocument[] = [];
-        const onDocumentCreated: (createdParts: PartDocument[]) => void = (createdParts: PartDocument[]) => {
-            activeParts = activeParts.concat(...createdParts);
-            callback(activeParts);
-        };
-        const onDocumentModified: (modifiedParts: PartDocument[]) => void = (modifiedParts: PartDocument[]) => {
-            const result: PartDocument[] = activeParts;
-            for (const modifiedPart of modifiedParts) {
-                result.forEach((part: PartDocument) => {
-                    if (part.id === modifiedPart.id) part.data = modifiedPart.data;
-                });
-            }
-            activeParts = result;
-            callback(activeParts);
-        };
-        const onDocumentDeleted: (deletedDocIds: PartDocument[]) => void = (deletedDocs: PartDocument[]) => {
-            const result: PartDocument[] = [];
-            for (const activePart of activeParts) {
-                if (deletedDocs.every((part: PartDocument) => part.id !== activePart.id)) {
-                    result.push(activePart);
+        const onDocumentCreated: (createdDocs: FirestoreDocument<Part>[]) => void =
+            (createdDocs: FirestoreDocument<Part>[]) => {
+                for (const createdDoc of createdDocs) {
+                    activeParts.push(new PartDocument(createdDoc.id, createdDoc.data));
                 }
-            }
-            activeParts = result;
-            callback(activeParts);
-        };
+                callback(activeParts);
+            };
+        const onDocumentModified: (modifiedDocs: FirestoreDocument<Part>[]) => void =
+            (modifiedDocs: FirestoreDocument<Part>[]) => {
+                const result: PartDocument[] = activeParts;
+                for (const modifiedDoc of modifiedDocs) {
+                    result.forEach((doc: FirestoreDocument<Part>) => {
+                        if (doc.id === modifiedDoc.id) doc.data = modifiedDoc.data;
+                    });
+                }
+                activeParts = result;
+                callback(activeParts);
+            };
+        const onDocumentDeleted: (deletedDocIds: FirestoreDocument<Part>[]) => void =
+            (deletedDocs: FirestoreDocument<Part>[]) => {
+                const result: PartDocument[] = [];
+                for (const activePart of activeParts) {
+                    if (deletedDocs.every((part: FirestoreDocument<Part>) => part.id !== activePart.id)) {
+                        result.push(activePart);
+                    }
+                }
+                activeParts = result;
+                callback(activeParts);
+            };
         const partObserver: FirestoreCollectionObserver<Part> =
             new FirestoreCollectionObserver(onDocumentCreated, onDocumentModified, onDocumentDeleted);
         return this.partDAO.observingWhere([['result', '==', MGPResult.UNACHIEVED.value]], partObserver);
