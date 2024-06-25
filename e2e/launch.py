8967ec2784a9fd6a25af24fc25c3e97bff178fb6
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-06-25 09:54:27 -0400

    mooaar

--- e2e/launch.py
+++ e2e/launch.py
@@ -26,7 +26,9 @@ def run_e2e():
     try:
         print('[e2e] Launching processes')
         frontend_process = subprocess.Popen(['npm', 'run', 'start:emulator'], stdout=subprocess.PIPE, universal_newlines=True)
-        backend_process = subprocess.Popen(['make', '-C', 'backend', 'run'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        backend_process = subprocess.Popen(['make', '-C', 'backend', 'run'])
+
+        print(requests.get('http://localhost:8081'))
 
         print('[e2e] Watching URLs')
         watch_thread = threading.Thread(target=open_urls, args=(frontend_process,))
