6c21c005cf9ae91f6d671935e3c89cfc27364818
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-06-25 10:59:54 -0400

    bli

--- e2e/launch.py
+++ e2e/launch.py
@@ -27,6 +27,7 @@ def run_e2e():
         print('[e2e] Launching processes')
         frontend_process = subprocess.Popen(['npm', 'run', 'start:emulator'], stdout=subprocess.PIPE, universal_newlines=True)
         backend_process = subprocess.Popen(['make', '-C', 'backend', 'run'])
+        time.sleep(10)
 
         print(requests.get('http://localhost:8081'))
 
