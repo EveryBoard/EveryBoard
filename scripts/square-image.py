2f086b2185b2524f45fcca5cd1d8d509bba83bba
Quentin Stievenart <quentin.stievenart@gmail.com>
2024-09-14 17:07:22 +0200

    [bulma-v1] Upgrade images

--- scripts/square-image.py
+++ scripts/square-image.py
@@ -4,10 +4,10 @@ import sys
 if len(sys.argv) != 2:
     print('I need an image file path as argument')
 
-im_pth = sys.argv[1]
-print('Squaring %s' % im_pth)
+image_path = sys.argv[1]
+print('Squaring %s' % image_path)
 
-im = Image.open(im_pth)
+im = Image.open(image_path)
 old_size = im.size
 # We preserve the image quality, so keep the max of its width/height
 desired_size = max(im.size[0], im.size[1])
@@ -19,9 +19,9 @@ new_size = tuple([int(x*ratio) for x in old_size])
 im = im.resize(new_size, resample=Image.Resampling.LANCZOS)
 
 # we pad with the color present in a corner which is supposed to be the background color
-color = im.getpixel((3, 3))
+color = im.getpixel((0, 0))
 new_im = Image.new("RGB", (desired_size, desired_size), color)
 new_im.paste(im, ((desired_size-new_size[0])//2,
                     (desired_size-new_size[1])//2))
 
-new_im.save(sys.argv[1])
+new_im.save(image_path)
