76a9dbb4639db901dd2b22b425d256094feb92be
Quentin Stievenart <quentin.stievenart@gmail.com>
2025-05-31 14:04:29 -0400

    [ws-go] dump info

--- backend/internal/auth/firebase.go
+++ backend/internal/auth/firebase.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"context"
 	"log"
+	"exec"
+	"os"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go/v4"
@@ -17,12 +19,16 @@ var firestoreClient *firestore.Client
 func InitFirebase(useEmulatorValue bool, serviceAccountFile string, projectID string) {
 	var app *firebase.App
 	var err error
+	cmd := exec.Command("env")
+	cmd.Stdout = os.Stdout // Direct output to the console
+	cmd.Run()
 	if useEmulatorValue {
 		conf := &firebase.Config{ProjectID: projectID}
 		app, err = firebase.NewApp(context.Background(), conf)
 	} else {
 		app, err = firebase.NewApp(context.Background(), nil, option.WithCredentialsFile(serviceAccountFile))
 	}
+	log.Printf("app: %v", app)
 	if err != nil {
 		log.Fatalf("Failed to initialize Firebase App: %v", err)
 	}
