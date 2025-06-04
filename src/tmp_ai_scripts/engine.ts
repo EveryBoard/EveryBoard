db6cb3dba81bf6629e3031870bd1825a41c3b2d2
Martin Remy <martin.remy.42@gmail.com>
2025-06-04 08:27:14 +0200

    build broken; about to try something

--- src/tmp_ai_scripts/engine.ts
+++ src/tmp_ai_scripts/engine.ts
@@ -7,23 +7,29 @@ import { TableUtils } from '../app/jscaip/TableUtils';
 import { MancalaMove } from '../app/games/mancala/common/MancalaMove';
 import { MancalaState } from '../app/games/mancala/common/MancalaState';
 import { PlayerNumberMap } from '../app/jscaip/PlayerMap';
-import { MancalaNode } from '../app/games/mancala/common/MancalaRules';
 import { P4State } from '../app/games/p4/P4State';
-import { P4Config, P4Node, P4Rules } from '../app/games/p4/P4Rules';
+import { P4Config, P4Rules } from '../app/games/p4/P4Rules';
 import { P4Move } from '../app/games/p4/P4Move';
 import { P4MoveGenerator } from '../app/games/p4/P4MoveGenerator';
+import { GameState } from '../app/jscaip/state/GameState';
+import { GameNode } from '../app/jscaip/AI/GameNode';
+import { Move } from '../app/jscaip/Move';
+import { GameInfo } from '../app/components/normal-component/pick-game/game-info';
+import { RulesConfig } from '../app/jscaip/RulesConfigUtil';
+import { MoveGenerator } from '../app/jscaip/AI/AI';
 
 // compile with "npx tsc -p tsconfig.jscaip.json"
-const rules: P4Rules = P4Rules.get();
-const config: MGPOptional<P4Config> = rules.getDefaultRulesConfig();
+globalThis.$localize = (string) => "localize(" + string + ")";
+$localize = (string) => "localize(" + string + ")";
 
 interface Input {
     action: 'choose' | 'getGameStatus' | 'getLegalMoves' | 'getGameName' | 'getInitialState';
-    gameState?: P4State;
+    gameName: string;
+    gameState?: GameState;
     move?: number;
 }
 
-function getP4Node(input: Input): P4State {
+function getP4State(input: Input): P4State {
     const board: PlayerOrNone[][] = TableUtils.map(
         input.gameState?.board,
         (p: PlayerOrNone) => PlayerOrNone.encoder.decode(p['value']),
@@ -31,14 +37,13 @@ function getP4Node(input: Input): P4State {
     const turn: number = input.gameState?.turn;
     return new P4State(board, turn);
 }
-function getMancalaNode(input: Input): MancalaNode {
+function getMancalaState(input: Input): MancalaState {
     const board: number[][] = input.gameState?.board;
     const turn: number = input.gameState?.turn;
     const playerZeroValue: number = input.gameState?.scores.map.map[0].value;
     const playerOneValue: number = input.gameState?.scores.map.map[0].value;
     const scores: PlayerNumberMap = PlayerNumberMap.of(playerZeroValue, playerOneValue);
-    const state: MancalaState = new MancalaState(board, turn, scores);
-    return new MancalaNode(state);
+    return new MancalaState(board, turn, scores);
 }
 function getMancalaMove(input: Input): MancalaMove {
     return MancalaMove.of(input.move!);
@@ -46,7 +51,6 @@ function getMancalaMove(input: Input): MancalaMove {
 function getP4Move(input: Input): P4Move {
     return P4Move.of(input.move!);
 }
-const getGameState = getP4Node;
 
 // Create readline interface for persistent communication
 const rl = readline.createInterface({
@@ -55,45 +59,77 @@ const rl = readline.createInterface({
     terminal: false,
 });
 
-rl.on('line', (line: string) => {
-    try {
-        const input: Input = JSON.parse(line);
-        let response: any;
+class ProcessApplier<S extends GameState, M extends Move> {
 
+    public readonly gameInfo: GameInfo;
+
+    public constructor(public readonly gameName: string,
+                       public readonly moveMapper: (input: Input) => M,
+                       public readonly gameStateMapper: (input: Input) => S,
+                       public readonly moveGenerator: MoveGenerator<M, S, RulesConfig>,
+    ) {
+        this.gameInfo = GameInfo.getByUrlName(gameName).get();
+    }
+    private getResponse(input: Input): any { // TODO: CHANGE return name
         if (input.action === 'choose') {
-            const gameState: P4State = getP4Node(input);
-            let node: P4Node = new P4Node(gameState);
-            const move: P4Move = getP4Move(input);
-            const result: MGPFallible<P4Node> = rules.choose(node, move, config);
+            const gameState: S = this.gameStateMapper(input);
+            let node: GameNode<M, S> = new GameNode<M, S>(gameState);
+            const move: M = this.moveMapper(input);
+            const defaultConfig: MGPOptional<RulesConfig> = this.gameInfo.rules.getDefaultConfig();
+            const result: MGPFallible<GameNode<M, S>> = this.gameInfo.rules.choose(node, move, defaultConfig);
             if (result.isSuccess()) {
                 node = result.get();
             }
-            response = {
+            return {
                 success: result.isSuccess(),
                 result: node.gameState,
             };
         } else if (input.action === 'getGameStatus') {
-            const gameState: P4State = getGameState(input);
-            const node: P4Node = new P4Node(gameState);
-            const status: GameStatus = rules.getGameStatus(node);
-            response = { status };
+            const gameState: S = this.gameStateMapper(input);
+            const node: GameNode<M, S> = new GameNode<M, S>(gameState);
+            const status: GameStatus = this.gameInfo.rules.getGameStatus(node);
+            return { status };
         } else if (input.action === 'getLegalMoves') {
-            const gameState: P4State = getGameState(input);
-            const node: P4Node = new P4Node(gameState);
-            const moveGenerator: P4MoveGenerator = new P4MoveGenerator();
-            const legalMoves: P4Move[] = moveGenerator.getListMoves(node, config);
-            response = { legalMoves };
-        } else if (input.action === 'getGameName') {
-            response = { name: rules.constructor.name };
+            const gameState: S = this.gameStateMapper(input);
+            const node: GameNode<M, S> = new GameNode<M, S>(gameState);
+            const moveGenerator: MoveGenerator<M, S, RulesConfig> = this.moveGenerator;
+            const defaultConfig: MGPOptional<RulesConfig> = this.gameInfo.rules.getDefaultConfig();
+            const legalMoves: M[] = moveGenerator.getListMoves(node, defaultConfig);
+            return { legalMoves };
         } else if (input.action === 'getInitialState') {
-            const initialNode: P4Node = rules.getInitialNode(config);
-            response = { initialState: initialNode.gameState };
+            // const optGameStateProvider: MGPOptional<(config: MGPOptional<RulesConfig>) => GameState> =
+                // GameInfo.getStateProvider(this.gameName);
+            // const gameStateProvider: (config: MGPOptional<RulesConfig>) => GameState = optGameStateProvider.get();
+            // const defaultConfig: MGPOptional<RulesConfig> = this.gameInfo.rules.getDefaultConfig();
+            const initialState: GameState = this.gameInfo.rules.getInitialState();
+            return { initialState };
         }
-
+    }
+    public applyRequest(input: Input): void {
         // Always send response as single line
-        const stringResponse: string = JSON.stringify(response);
-        console.log(stringResponse);
+        // const response: any = this.getResponse(input);
+        // const stringResponse: string = JSON.stringify(response);
+        // console.log(stringResponse + "\n");
+        console.log("jaaaajentouk\n");
         // process.stdout.write(stringResponse);
+    }
+}
+rl.on('line', (line: string) => {
+    try {
+        const input: Input = JSON.parse(line);
+        switch (input.gameName) {
+            case 'p4':
+                const processApplier: ProcessApplier<P4State, P4Move> = new ProcessApplier(
+                    'p4',
+                    getP4Move,
+                    getP4State,
+                    new P4MoveGenerator(),
+                );
+                processApplier.applyRequest(input);
+                break;
+            default:
+                throw new TypeError('Le sang de ta jaaj')
+        }
     } catch (e: any) {
         const errorMessage: string = JSON.stringify({ error: e?.message || e?.toString?.() || 'Unknown error' });
         console.error(errorMessage);
