15c3964ca13b0bdccd5e97d01920278c9f95f59a
Martin Remy <martin.remy.42@gmail.com>
2026-01-14 21:28:16 +0100

    small change

--- src/app/games/encapsule/EncapsulePiece.ts
+++ src/app/games/encapsule/EncapsulePiece.ts
@@ -2,7 +2,6 @@ import { ComparableObject, Encoder } from '@everyboard/lib';
 
 import { Player, PlayerOrNone } from '../../jscaip/Player';
 
-
 export class EncapsulePiece implements ComparableObject {
 
     public static encoder: Encoder<EncapsulePiece> = Encoder.tuple(
