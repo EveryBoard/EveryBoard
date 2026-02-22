b677eadd38ccb224dd53f9353e93fab140a740d5
Martin Remy <martin.remy.42@gmail.com>
2026-02-22 15:28:13 +0100

    move buging import one line up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,8 +35,8 @@ import { VerifyAccountComponent } from './components/normal-component/verify-acc
 import { WelcomeComponent } from './components/normal-component/welcome/welcome.component';
 import { DemoCardWrapperComponent } from './components/wrapper-components/demo-card-wrapper/demo-card-wrapper.component';
 import { LocalGameWrapperComponent } from './components/wrapper-components/local-game-wrapper/local-game-wrapper.component';
-import { OnlineGameWrapperComponent } from './components/wrapper-components/online-game-wrapper/online-game-wrapper.component';
 import { DirArrowComponent } from './components/game-components/arrow-component/dir-arrow.component'; // Moving this leads to bug
+import { OnlineGameWrapperComponent } from './components/wrapper-components/online-game-wrapper/online-game-wrapper.component';
 import { PartCreationComponent } from './components/wrapper-components/part-creation/part-creation.component';
 import { RulesConfigurationComponent } from './components/wrapper-components/rules-configuration/rules-configuration.component';
 import { TutorialGameWrapperComponent } from './components/wrapper-components/tutorial-game-wrapper/tutorial-game-wrapper.component';
