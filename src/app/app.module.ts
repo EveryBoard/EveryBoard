691b43e734079f8cfc7d22534d0019849de4ab95
Martin Remy <martin.remy.42@gmail.com>
2026-02-22 16:14:14 +0100

    move buging import one line up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,8 +26,8 @@ import { NextGameLoadingComponent } from './components/normal-component/next-gam
 import { NotFoundComponent } from './components/normal-component/not-found/not-found.component';
 import { OnlineGameCreationComponent } from './components/normal-component/online-game-creation/online-game-creation.component';
 import { OnlineGameSelectionComponent } from './components/normal-component/online-game-selection/online-game-selection.component';
-import { PickGameComponent } from './components/normal-component/pick-game/pick-game.component';
 import { DirArrowComponent } from './components/game-components/arrow-component/dir-arrow.component'; // Moving this leads to bug
+import { PickGameComponent } from './components/normal-component/pick-game/pick-game.component';
 import { RegisterComponent } from './components/normal-component/register/register.component';
 import { ResetPasswordComponent } from './components/normal-component/reset-password/reset-password.component';
 import { SettingsComponent } from './components/normal-component/settings/settings.component';
