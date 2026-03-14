bdc020d139daa9904424b040fae8cdb661fb3b8f
Martin Remy <martin.remy.42@gmail.com>
2026-03-14 21:18:09 +0100

    remove sahara related comment

--- src/app/games/sahara/SaharaHeuristic.ts
+++ src/app/games/sahara/SaharaHeuristic.ts
@@ -10,9 +10,6 @@ import { SaharaState } from './SaharaState';
 export class SaharaHeuristic extends PlayerMetricHeuristic<SaharaMove, SaharaState> {
 
     public override getMetrics(node: SaharaNode, _config: NoConfig): PlayerNumberTable {
-        // TODO: ajouter la métrique "is surrounded", quand une pièce, même en bougeant autant qu'elle veut, ne peux que atteindre des cases ennemis
-        // en gros, si cette pièce était morte elle serait considérée comme étant en territoire ennemi
-        // Cette métrique devrais prendre en compte que si "je forme un terrritoire" avec une autre pièce, je suis invincible
         const zeroFreedoms: number[] = SaharaRules.getBoardValuesFor(node.gameState, Player.ZERO);
         const oneFreedoms: number[] = SaharaRules.getBoardValuesFor(node.gameState, Player.ONE);
         return PlayerNumberTable.of(zeroFreedoms, oneFreedoms);
