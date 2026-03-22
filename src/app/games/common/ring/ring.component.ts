261f1479ec7e25ed66e4e4c34dc4ecb1dc47319b
Martin Remy <martin.remy.42@gmail.com>
2026-03-22 21:08:48 +0100

    extracted ring and used them in yinsh

---
+++ src/app/games/common/ring/ring.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: '[app-ring]',
+    templateUrl: './ring.component.svg',
+    styleUrls: ['../../../components/game-components/game-component/game-component.scss'],
+})
+export class RingComponent {
+
+    @Input() ringOuterSize: number;
+
+    @Input() ringMidSize: number;
+
+    @Input() ringInnerSize: number;
+
+    @Input() midRingClasses: string[];
+
+}
