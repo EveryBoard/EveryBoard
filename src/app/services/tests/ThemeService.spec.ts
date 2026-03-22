ca8107a986a3b132804d710287eb19fd4543eb0d
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-03-22 19:23:07 -0400

    upgrade to angular v20

--- src/app/services/tests/ThemeService.spec.ts
+++ src/app/services/tests/ThemeService.spec.ts
@@ -1,5 +1,6 @@
+import { DOCUMENT } from '@angular/core';
 /* eslint-disable max-lines-per-function */
-import { DOCUMENT } from '@angular/common';
+
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { WelcomeComponent } from '../../components/normal-component/welcome/welcome.component';
