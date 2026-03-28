3665225e61d8546ad7e05a1180ac866da55b2304
Martin Remy <martin.remy.42@gmail.com>
2026-03-28 21:09:56 +0100

    adapted yinsh to new ring format

--- src/app/games/common/ring/ring.component.ts
+++ src/app/games/common/ring/ring.component.ts
@@ -1,18 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit } from '@angular/core';
 
 @Component({
     selector: '[app-ring]',
     templateUrl: './ring.component.svg',
     styleUrls: ['../../../components/game-components/game-component/game-component.scss'],
 })
-export class RingComponent {
+export class RingComponent implements AfterViewInit {
 
-    @Input() ringOuterSize: number;
+    @Input() strokeWidth: number;
 
-    @Input() ringMidSize: number;
+    @Input() width: number;
 
-    @Input() ringInnerSize: number;
+    @Input() strokeColor: string = 'black';
 
     @Input() midRingClasses: string | string[];
 
+    public outsideStrokeRadius: number;
+
+    public midRingRadius: number;
+
+    public insideStrokeRadius: number;
+
+    public ngAfterViewInit(): void {
+        const outsideStrokeInsideRadius: number = 100 - this.strokeWidth;
+        this.outsideStrokeRadius = outsideStrokeInsideRadius + (this.strokeWidth / 2);
+        const midRindInsideRadius: number = outsideStrokeInsideRadius - this.width;
+        this.midRingRadius = midRindInsideRadius + (this.width / 2);
+        const insideStrokeInsideRadius: number = midRindInsideRadius - this.strokeWidth;
+        this.insideStrokeRadius = insideStrokeInsideRadius + (this.strokeWidth / 2);
+    }
 }
