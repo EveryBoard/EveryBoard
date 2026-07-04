d5533b6a252c66f3e6c9cbc3463162df82221f62
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-07-04 12:16:03 -0400

    fix tests

--- src/app/components/wrapper-components/online-game-wrapper/online-game-wrapper.component.spec.ts
+++ src/app/components/wrapper-components/online-game-wrapper/online-game-wrapper.component.spec.ts
@@ -210,7 +210,7 @@ describe('OnlineGameWrapperComponent Lifecycle', () => {
 
     it('should register timers only once', fakeAsync(async() => {
         // Given a started game
-        const timeManager: OGWCTimeManagerService = TestBed.inject(OGWCTimeManagerService);
+        const timeManager: OGWCTimeManagerService = testUtils['fixture'].debugElement.injector.get(OGWCTimeManagerService);
         spyOn(timeManager, 'setTimers').and.callThrough();
         await prepareComponent(true);
 
