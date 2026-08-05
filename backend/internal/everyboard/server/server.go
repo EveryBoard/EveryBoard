980f0fbe906a1c58ddc35d60f81b1d1e21d2bdb9
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-08-05 10:24:19 -0400

    Bump version

--- backend/internal/everyboard/server/server.go
+++ backend/internal/everyboard/server/server.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-const Version = "1.0.3.1"
+const Version = "1.0.3.2"
 
 func cors(origin string, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
