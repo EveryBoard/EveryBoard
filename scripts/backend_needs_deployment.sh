9f5beb0090933a240348ba2fbaef5fd831e99342
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-08-10 10:38:46 -0400

    more debug in deployment

--- scripts/backend_needs_deployment.sh
+++ scripts/backend_needs_deployment.sh
@@ -3,5 +3,15 @@
 CURRENT_VERSION=$(cd backend && make version)
 # Get the deployed version number, where the server is provided as an argument to the script
 DEPLOYED_VERSION=$(curl -s "$1"/version)
+
+printf 'Backend version check: source=%s, deployed=%s\n' \
+    "$CURRENT_VERSION" "$DEPLOYED_VERSION"
+
 # If versions differ, we need to redeploy
-test "$CURRENT_VERSION" != "$DEPLOYED_VERSION"
+if [ "$CURRENT_VERSION" = "$DEPLOYED_VERSION" ]; then
+    echo "Backend versions match; skipping deployment."
+    exit 1
+fi
+
+echo "Backend versions differ; deployment required."
+exit 0
