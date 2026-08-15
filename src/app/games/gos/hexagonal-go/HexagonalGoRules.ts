98db921ec7cd0dc5842c77e1de9b1750006c5a8b
Martin Remy <martin.remy.42@gmail.com>
2026-08-15 18:31:54 +0200

    add import rules for src/ that got passed our radars

--- src/app/games/gos/hexagonal-go/HexagonalGoRules.ts
+++ src/app/games/gos/hexagonal-go/HexagonalGoRules.ts
@@ -1,10 +1,10 @@
-import { TableUtils } from 'src/app/jscaip/TableUtils';
 
 import { MGPOptional } from '@everyboard/lib';
 
 import { NumberConfig, RulesConfigDescription, RulesConfigDescriptionLocalizable } from '../../../components/wrapper-components/rules-configuration/RulesConfigDescription';
 import { PlayerNumberMap } from '../../../jscaip/PlayerMap';
 import { RulesConfig } from '../../../jscaip/RulesConfigUtil';
+import { TableUtils } from '../../../jscaip/TableUtils';
 import { MGPValidators } from '../../../utils/MGPValidator';
 import { AbstractGoRules } from '../AbstractGoRules';
 import { GoGroupDataFactory, HexagonalGoGroupDataFactory } from '../GoGroupDataFactory';
