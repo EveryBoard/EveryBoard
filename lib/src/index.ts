dc51b91e330fb72421957f5def0ba73a243374ff
Martin Remy <martin.remy.42@gmail.com>
2026-08-15 18:47:59 +0200

    fix build

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,6 +5,7 @@ export * from './Encoder';
 export * from './JSON';
 export * from './MathUtils';
 export * from './MGPFallible';
+export * from './MGPMap';
 export * from './ReversibleMap';
 export * from './MGPOptional';
 export * from './Set';
