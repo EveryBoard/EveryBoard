b79608fe73151dfc3ec9880cf446d61af88e0391
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-08-16 20:00:28 -0400

    Simplify backend deployment script

--- scripts/backend_needs_deployment.sh
+++ scripts/backend_needs_deployment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-# Get the curent version of the backend from the source code (in the form of let version_number : string = "1.0.0" for example)
+# Get the current version of the backend from the source code
 CURRENT_VERSION=$(cd backend && make version)
 # Get the deployed version number, where the server is provided as an argument to the script
 DEPLOYED_VERSION=$(curl -s "$1"/version)
 
-printf 'Backend version check: source=%s, deployed=%s\n' \
-    "$CURRENT_VERSION" "$DEPLOYED_VERSION"
+echo "Backend version check: source=$CURRENT_VERSION, deployed=$DEPLOYED_VERSION"
 
 # If versions differ, we need to redeploy
 if [ "$CURRENT_VERSION" = "$DEPLOYED_VERSION" ]; then
