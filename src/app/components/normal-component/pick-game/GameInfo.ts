c24ac1b2ebff546304001212bf200949c2742e32
Martin Remy <martin.remy.42@gmail.com>
2026-08-20 15:23:32 +0200

    rerun translation and bump version

--- src/app/components/normal-component/pick-game/GameInfo.ts
+++ src/app/components/normal-component/pick-game/GameInfo.ts
@@ -218,7 +218,7 @@ export class GameInfo {
             new GameInfo($localize`Hexagonal Go`,           'HexagonalGo',           HexagonalGoComponent,           new HexagonalGoTutorial(),           HexagonalGoRules.get(),           new Date('2026-02-14'), GameDescription.HEXAGONAL_GO()          ), // 42:                             * Martin
             new GameInfo($localize`Toric Reversi`,          'ToricReversi',          ToricReversiComponent,          new ToricReversiTutorial(),          ToricReversiRules.get(),          new Date('2026-08-10'), GameDescription.TORIC_REVERSI()         ), // 43:                             * Martin
             new GameInfo($localize`Bashni`,                 'Bashni',                BashniComponent,                new BashniTutorial(),                BashniRules.get(),                new Date('2026-08-17'), GameDescription.BASHNI()                ), // 44:                             * Quentin
-            new GameInfo($localize`Zoomed Go`,              'ZoomedGo',              ZoomedGoComponent,              new ZoomedGoTutorial(),              ZoomedGoRules.get(),              new Date('2026-08-19'), GameDescription.ZOOMED_GO()             ), // 45:                             * Martin
+            new GameInfo($localize`Zoomed Go`,              'ZoomedGo',              ZoomedGoComponent,              new ZoomedGoTutorial(),              ZoomedGoRules.get(),              new Date('2026-08-20'), GameDescription.ZOOMED_GO()             ), // 45:                             * Martin
         ].sort((a: GameInfo, b: GameInfo) => a.name.localeCompare(b.name));
         // After Apagos: median = 26d; average = 53d
         // 9d 10d 12d 13d 18d - 18d 20d 22d 25d 26d - (26d) - 49d 65d 71d 76d 93d - 94j 4m 4m 7m 11m
