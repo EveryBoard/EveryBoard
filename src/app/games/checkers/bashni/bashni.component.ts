c548758e4359bc008eb11563465de57a8c8f0d8e
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-08-20 09:21:17 -0400

    PR comments

--- src/app/games/checkers/bashni/bashni.component.ts
+++ src/app/games/checkers/bashni/bashni.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { CheckersComponent } from '../common/checkers.component';
 
 import { BashniRules } from './BashniRules';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'app-bashni',
     templateUrl: '../common/checkers.component.html',
     styleUrls: ['../../../components/game-components/game-component/game-component.scss'],
