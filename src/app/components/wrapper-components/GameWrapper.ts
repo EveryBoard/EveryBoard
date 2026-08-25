98f7b4acc144537ef66a23b91117022e3b3f00e0
Martin Remy <martin.remy.42@gmail.com>
2026-08-25 19:07:41 +0200

    linter fix attempt

--- src/app/components/wrapper-components/GameWrapper.ts
+++ src/app/components/wrapper-components/GameWrapper.ts
@@ -3,16 +3,16 @@ import { Router } from '@angular/router';
 
 import { Comparable, MGPFallible, MGPOptional, MGPValidation, Utils } from '@everyboard/lib';
 
-import { GameInfo } from '../normal-component/pick-game/GameInfo';
-import { Move } from '../../jscaip/Move';
-import { Player, PlayerOrNone } from '../../jscaip/Player';
 import { PlayerMap } from '../../jscaip/PlayerMap';
 import { AbstractNode } from '../../jscaip/AI/GameNode';
+import { Player, PlayerOrNone } from '../../jscaip/Player';
+import { Move } from '../../jscaip/Move';
 import { RulesConfig, RulesConfigUtils } from '../../jscaip/RulesConfigUtil';
 import { MessageDisplayer } from '../../services/MessageDisplayer';
 import { Localized } from '../../utils/LocaleUtils';
 import { AbstractGameComponent } from '../game-components/game-component/AbstractGameComponent';
 import { AnyFunction, ClickNamer } from '../game-components/game-component/ClickHandler';
+import { GameInfo } from '../normal-component/pick-game/GameInfo';
 
 import { BaseWrapperComponent } from './BaseWrapperComponent';
 
