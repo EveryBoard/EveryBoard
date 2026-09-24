1d5dd112a0a5f4c592a7b1cf2bfd66d46289a834
Quentin Stievenart <quentin.stievenart@gmail.com>
2026-09-24 16:04:55 +0200

    fix

--- games/src/coverage-setup.ts
+++ games/src/coverage-setup.ts
@@ -1,5 +1,2 @@
-declare function require(moduleName: string): unknown;
-
 // Load the entire package so coverage includes files that individual specs do not import.
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require('./index');
+import './index';
